485053c0958fff5d4846e68d76e1795cf94c4cbe
NatySousa <nataliaspsousa@gmail.com>
2021-05-02 15:10:27 -0300

    versão inicial do projeto

---
+++ Lista04/Program.cs
@@ -0,0 +1,91 @@
+﻿using Lista04.Entities;
+using Lista04.Repositories;
+using System;
+using System.Linq;
+
+namespace Lista04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n*** CADASTRO DE CLIENTES ***\n");
+
+            try
+            {
+                var cliente = new Cliente();
+
+                cliente.IdCliente = Guid.NewGuid();
+
+                Console.Write("\nInforme o nome do cliente: ");
+                cliente.Nome = Console.ReadLine();
+
+                Console.Write("\nInforme o cpf do cliente: ");
+                cliente.Cpf = Console.ReadLine();
+
+                Console.Write("\nInforme a data de nascimento do cliente: ");
+                cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("\nInforme o email do cliente: ");
+                cliente.Email = Console.ReadLine();
+
+                var clienteRepository = new ClienteRepository();
+                clienteRepository.Inserir(cliente);
+
+                Console.WriteLine("\nCliente cadastrado com sucesso! ");
+               
+                Console.WriteLine("\n*** CONSULTA DE CLIENTES ***\n ");
+
+                foreach (var item in clienteRepository.Consultar())
+                {
+
+                    Console.Write("\nId do Cliente..........................:" + item.IdCliente);
+                    Console.Write("\nNome do Cliente........................:" + item.Nome);
+                    Console.Write("\nCpf do Cliente.........................:" + item.Cpf);
+                    Console.Write("\nData de Nascimento do Cliente..........:" + item.DataNascimento);
+                    Console.Write("\nEmail do Cliente.......................:" + item.Email);
+                    Console.WriteLine();
+
+                }
+               
+
+                Console.Write("\nEmail invalido.Por favor informe um novo email: ");
+                cliente.Email = Console.ReadLine();
+
+                clienteRepository.Atualizar(cliente);
+
+                Console.WriteLine("\n*** CONSULTA DE CLIENTES ***\n ");
+
+                foreach (var item in clienteRepository.Consultar())
+                {
+
+                    Console.Write("\nId do Cliente..........................:" + item.IdCliente);
+                    Console.Write("\nNome do Cliente........................:" + item.Nome);
+                    Console.Write("\nCpf do Cliente.........................:" + item.Cpf);
+                    Console.Write("\nData de Nascimento do Cliente..........:" + item.DataNascimento);
+                    Console.Write("\nEmail do Cliente.......................:" + item.Email);
+                    Console.WriteLine();
+                }
+                Console.WriteLine("\nDeseja excluir o ultimo cliente cadastrado? Sim ou Não? ");
+
+                if (Console.ReadLine()=="Sim")
+                {
+                    clienteRepository.Excluir(cliente);
+
+                    Console.WriteLine("\nCadastro excluido com sucesso! ");
+                }
+
+
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine("\nErro: " + e.Message);
+            }
+
+            Console.ReadKey();
+        }   
+
+
+    }
+}
---
+++ Lista04/Repositories/ClienteRepository.cs
@@ -0,0 +1,91 @@
+﻿using Lista04.Entities;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+
+namespace Lista04.Repositories
+{
+    public class ClienteRepository
+    {
+        private string connectionstring = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDLista04;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public void Inserir(Cliente cliente)
+        {
+            var sql = @"
+                    INSERT INTO CLIENTE(IDCLIENTE, NOME, CPF, DATANASCIMENTO, EMAIL)
+                    VALUES(@IdCliente, @Nome, @Cpf, @DataNascimento, @Email)
+                ";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                connection.Execute(sql, cliente);
+            }
+
+        }   
+        public void Atualizar (Cliente cliente)
+        {
+            var sql = @"
+                    UPDATE CLIENTE
+                    SET
+                       NOME = @Nome,
+                       CPF = @Cpf,
+                       DATANASCIMENTO = @DataNascimento,
+                       EMAIL = @Email 
+                   WHERE
+                       IDCLIENTE = @IdCliente
+                 ";
+
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                connection.Execute(sql, cliente);
+
+            }
+             
+
+        }
+        
+        public void Excluir (Cliente cliente)
+        {
+            var sql = @"
+                    DELETE FROM CLIENTE
+                    WHERE IDCLIENTE = @IdCliente
+               ";
+
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                connection.Execute(sql, cliente);
+            }
+        }
+
+
+        public List<Cliente> Consultar()
+        { 
+            var sql = @" 
+                     SELECT * FROM CLIENTE
+                     ORDER BY NOME
+              ";
+            using ( var connection = new SqlConnection(connectionstring))
+            {
+                return connection.Query<Cliente>(sql).ToList();
+            }
+
+        }
+
+        public Cliente ConsultarEmail(Cliente cliente)
+        {
+            var sql = @" 
+                     SELECT * FROM CLIENTE
+                     WHERE EMAIL = @Email
+                     ORDER BY NOME
+              ";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                return connection.Query<Cliente>(sql).FirstOrDefault();
+            }
+
+        }
+    }
+}
+
+
+
